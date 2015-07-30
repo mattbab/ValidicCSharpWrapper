8412884e0f81008b3e10574936a4fb4e59b4ccf5
Alan <ak61us@gmail.com>
2015-07-30 09:01:08 -0700

    Added Fitness Data View

--- Validic.Mobile.DemoApp/Validic.Mobile.DemoApp/Views/ListViewDemoPage.cs
+++ Validic.Mobile.DemoApp/Validic.Mobile.DemoApp/Views/ListViewDemoPage.cs
@@ -5,13 +5,36 @@ using Xamarin.Forms;
 
 namespace Validic.Mobile.DemoApp.Views
 {
+    public static class GridHelper
+    {
+
+        public static void AddRow(this Grid grid, int col, int row, string path, string stringFormat = null)
+        {
+            // grid.Children.Add(item, Col, Row);
+            grid.Children.Add(new Label { Text = path }, col, row);
+            grid.Children.Add(CreateLabel(path, stringFormat), col, row);
+        }
+
+        public static Label CreateLabel(string path, string stringFormat = null)
+        {
+            var label = new Label();
+            label.SetBinding(Label.TextProperty, new Binding(path, BindingMode.OneWay, null, null, stringFormat));
+            return label;
+        }
+    }
+
     internal class ListViewDemoPage : ContentPage
     {
         private readonly ILog _log = LogManager.GetLogger("ListViewDemoPage");
 
-        private readonly ListView _listView;
+        private ListView _listView;
 
         public ListViewDemoPage()
+        {
+            CreateListView();
+        }
+
+        public void CreateListView()
         {
             var header = new Label
             {
@@ -50,21 +73,47 @@ namespace Validic.Mobile.DemoApp.Views
 
         private object LoadTemplate()
         {
-            if (_lastTemplate != null)
-                return _lastTemplate;
-
             _log.Debug("LoadTemplate");
+            if (_lastTemplate == null)
+                CreateMeTemplate();
 
-            // Create views with bindings for displaying each property.
-            var nameLabel = new Label();
-            nameLabel.SetBinding(Label.TextProperty, new Binding("Me.Id", BindingMode.OneWay));
 
-            //var birthdayLabel = new Label();
-            //birthdayLabel.SetBinding(Label.TextProperty, new Binding("Birthday", BindingMode.OneWay, null, null, "Born {0:d}"));
+            return _lastTemplate;
+        }
 
-            //var boxView = new BoxView();
-            //boxView.SetBinding(BoxView.ColorProperty, "FavoriteColor");
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            _log.Debug("[OnAppearing] : Title = {0}", Title);
+            var model = BindingContext as MainViewModel;
+            if (model == null)
+                return;
 
+            var title = Title.ToUpper();
+            switch (title)
+            {
+                case "ME":
+                    await model.SelectedMainRecord.GetOrganizationMeDataAsync();
+                    CreateMeTemplate();
+                    _listView.RowHeight = 40;
+                    _listView.ItemsSource = model.SelectedMainRecord.MeData;
+                    _log.Debug("_listView.ItemsSource = model.SelectedMainRecord.MeData: Finish");
+                    break;
+
+                case "FITNESS":
+                    await model.SelectedMainRecord.GetOrganizationFitnessData();
+                    CreateFitnessTemplate();
+                    _listView.RowHeight = 300;
+                    _listView.ItemsSource = model.SelectedMainRecord.FitnessData;
+                    _log.Debug("_listView.ItemsSource = model.SelectedMainRecord.MeData: Finish");
+                    break;
+            }
+        }
+
+
+
+        private void CreateMeTemplate()
+        {
             // Return an assembled ViewCell.
             _lastTemplate = new ViewCell
             {
@@ -82,32 +131,105 @@ namespace Validic.Mobile.DemoApp.Views
                             Spacing = 0,
                             Children =
                             {
-                                nameLabel
+                                GridHelper.CreateLabel("Me.Id")
                                 //birthdayLabel
                             }
                         }
                     }
                 }
             };
-            return _lastTemplate;
         }
 
-        protected override async void OnAppearing()
+        private void CreateFitnessTemplate()
         {
-            base.OnAppearing();
-            _log.Debug("[OnAppearing] : Title = {0}", Title);
-            var title = Title;
-            if (title.ToUpper() == "ME")
+            var grid = new Grid
             {
-                var model = BindingContext as MainViewModel;
-                if (model == null)
-                    return;
+                Padding = new Thickness(5, 10, 0, 0),
+                ColumnDefinitions =
+                {
+                    new ColumnDefinition {Width = new GridLength(100, GridUnitType.Absolute)},
+                    new ColumnDefinition {Width = new GridLength(1, GridUnitType.Star)},
+                },
+                RowDefinitions =
+                {
+                    new RowDefinition {Height = GridLength.Auto},
+                    new RowDefinition {Height = GridLength.Auto},
+                    new RowDefinition {Height = GridLength.Auto},
+                    new RowDefinition {Height = GridLength.Auto},
+                    new RowDefinition {Height = GridLength.Auto},
+                    new RowDefinition {Height = GridLength.Auto},
+                    new RowDefinition {Height = GridLength.Auto},
+                    new RowDefinition {Height = GridLength.Auto},
+                    new RowDefinition {Height = GridLength.Auto},
+                    new RowDefinition {Height = GridLength.Auto},
+                    new RowDefinition {Height = GridLength.Auto},
+                    new RowDefinition {Height = GridLength.Auto},
+                    new RowDefinition {Height = GridLength.Auto},
+                    new RowDefinition {Height = GridLength.Auto},
+                    new RowDefinition {Height = GridLength.Auto},
+                },
+            };
+
+            // Mesasurment
+            // Mesasurment
+            grid.AddRow(0, 0, "Id");
+            grid.AddRow(0, 1, "Time", "{0:MM/dd/yyy hh:mm:ss tt}");
+            grid.AddRow(0, 2, "Timestamp", "{0:MM/dd/yyy hh:mm:ss tt}");
+            grid.AddRow(0, 3, "UtcOffset");
+            grid.AddRow(0, 4, "LastUpdated", "{0:MM/dd/yyy hh:mm:ss tt}");
+            grid.AddRow(0, 5, "Source");
+            grid.AddRow(0, 6, "SourceName");
+            grid.AddRow(0, 7, "Extras");
+            grid.AddRow(0, 8, "UserId");
+            // Fitness  0,
+            grid.AddRow(0, 9, "Type");
+            grid.AddRow(0, 10, "Intensity");
+            grid.AddRow(0, 11, "StartTime", "{0:MM/dd/yyy hh:mm:ss tt}");
+            grid.AddRow(0, 12, "Distance", "{0:0.##}");
+            grid.AddRow(0, 13, "Duration");
+            grid.AddRow(0, 14, "Calories", "{0:0.##}");
 
 
-                await model.SelectedMainRecord.GetOrganizationMeDataAsync();
-                _listView.ItemsSource = model.SelectedMainRecord.MeData;
-                _log.Debug("_listView.ItemsSource = model.SelectedMainRecord.MeData: Finish");
-            }
+
+            _lastTemplate = new ViewCell
+            {
+                View = grid
+            };
+        }
+        private void CreateCompactFitnessTemplate()
+        {
+            var grid = new Grid
+            {
+                Padding = new Thickness(5, 10, 0, 0),
+                ColumnDefinitions =
+                {
+                    new ColumnDefinition {Width = new GridLength(100, GridUnitType.Absolute)},
+                    new ColumnDefinition {Width = new GridLength(100, GridUnitType.Absolute)},
+                    new ColumnDefinition {Width = new GridLength(100, GridUnitType.Absolute)},
+                    new ColumnDefinition {Width = new GridLength(100, GridUnitType.Absolute)},
+                    new ColumnDefinition {Width = new GridLength(100, GridUnitType.Absolute)},
+                    new ColumnDefinition {Width = new GridLength(100, GridUnitType.Absolute)},
+                    new ColumnDefinition {Width = new GridLength(100, GridUnitType.Absolute)},
+                },
+                RowDefinitions =
+                {
+                    new RowDefinition {Height = GridLength.Auto},
+                },
+            };
+
+            // Mesasurment
+            // Fitness  0,
+            grid.AddRow(0,0, "Type");
+            grid.AddRow(1,0, "Intensity");
+            grid.AddRow(2,0, "StartTime", "{0:MM/dd/yyy hh:mm:ss tt}");
+            grid.AddRow(3,0, "Distance", "{0:0.##}");
+            grid.AddRow(4,0, "Duration");
+            grid.AddRow(5,0, "Calories", "{0:0.##}");
+
+            _lastTemplate = new ViewCell
+            {
+                View = grid
+            };
         }
     }
 }
