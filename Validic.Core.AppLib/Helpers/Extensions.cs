22ef24f897dca2a6a939c1c5d71a62de426359f6
Alan <ak61us@gmail.com>
2015-08-02 13:35:52 -0700

    Moved extension class

---
+++ Validic.Core.AppLib/Helpers/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Validic.Mobile.DemoApp.Helpers;
+
+namespace Validic.Core.AppLib.Helpers
+{
+    public static class Extensions
+    {
+        public static void Add(this List<BindInfo> list, string path, string stringFormat = null)
+        {
+            list.Add(new BindInfo {Name = path, Binding = stringFormat});
+        }
+
+        // Alan
+        public static bool IsNullOrEmpty<T>(this IList<T> list)
+        {
+            return list != null && list.Count != 0;
+        }
+    }
+}
