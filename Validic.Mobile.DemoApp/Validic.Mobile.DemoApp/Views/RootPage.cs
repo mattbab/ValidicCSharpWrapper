23fd4a0e8b983c815f0b307bdba0fc79bcf5357e
Alan <ak61us@gmail.com>
2015-09-14 12:42:24 -0700

    Moved RootPage into Views

---
+++ Validic.Mobile.DemoApp/Validic.Mobile.DemoApp/Views/RootPage.cs
@@ -0,0 +1,21 @@
+﻿using Validic.Mobile.DemoApp.Menu;
+using Xamarin.Forms;
+
+namespace Validic.Mobile.DemoApp.Views
+{
+	public class RootPage : MasterDetailPage
+	{
+		public RootPage ()
+		{			
+			var menuPage = new MenuPage ();
+			menuPage.OnMenuTap = (page) => { 
+				IsPresented = false;
+				Detail = new NavigationPage (page);
+			};
+
+			Master = menuPage;
+			Detail = new NavigationPage(new MainRecordView ());
+        }
+    }
+}
+
